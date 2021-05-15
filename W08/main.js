c3a070ec37170f71f8aa736269f66d537bbd5bf0
SONG-BOYA <82433073+SONG-BOYA@users.noreply.github.com>
2021-05-15 16:00:37 +0900

    Update main.js

--- W08/main.js
+++ W08/main.js
@@ -7,3 +7,14 @@ const yscale = d3.scaleBand()
 .domain(data.map(d => d.label))
 .range([0, inner_height])
 .paddingInner(0.1);
+
+var chart = svg.append('g')
+.attr('transform', `translate(${margin.left}, ${margin.top})`);
+...
+
+chart.selectAll("rect").data(data).enter()
+.append("rect")
+.attr("x", 0)
+.attr("y", d => yscale(d.label))
+.attr("width", d => xscale(d.value))
+.attr("height", yscale.bandwidth());
