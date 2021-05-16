f1a5f442db58305602bd0abb1e89f7743e6274cb
SONG-BOYA <82433073+SONG-BOYA@users.noreply.github.com>
2021-05-16 17:29:02 +0900

    Update task3.js

--- W08/task3.js
+++ W08/task3.js
@@ -18,7 +18,7 @@ var svg = d3.select('#drawing_region')
 
 const pie = d3.pie()
       .value( d => d.value )
-      .label(d => d.lable);
+      .domain(data.map(d => d.lable));
       
 const arc = d3.arc()
       .innerRadius(radius/2)
