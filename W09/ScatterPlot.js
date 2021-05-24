5864f836814a4fb83cba661ae7396268f76cc4b3
SONG-BOYA <82433073+SONG-BOYA@users.noreply.github.com>
2021-05-24 14:18:28 +0900

    Update ScatterPlot.js

--- W09/ScatterPlot.js
+++ W09/ScatterPlot.js
@@ -13,7 +13,32 @@ class ScatterPlot {
         this.data = data;
         this.init();
     }
+let circles = svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle');
 
+circles
+    .attr('cx', d => d.x)
+    .attr('cy', d => d.y)
+    .attr('r', d => d.r);
+
+circles
+    .on('mouseover', (e,d) => {
+        d3.select('#tooltip')
+            .style('opacity', 1)
+            .html(`<div class="tooltip-label">Position</div>(${d.x}, ${d.y})`);
+    })
+    .on('mousemove', (e) => {
+        const padding = 10;
+        d3.select('#tooltip')
+            .style('left', (e.pageX + padding) + 'px')
+            .style('top', (e.pageY + padding) + 'px');
+    })
+    .on('mouseleave', () => {
+        d3.select('#tooltip')
+            .style('opacity', 0);
+    });
     init() {
         let self = this;
 
